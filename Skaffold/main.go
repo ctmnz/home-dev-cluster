758fc7092e58441792554583c795f4ff48ea2ffd
Daniel Stoinov <danie.stoinov@gmail.com>
2023-09-22 21:15:22 +0300

    added tls to app01

--- Skaffold/main.go
+++ Skaffold/main.go
@@ -25,6 +25,9 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
   <p>This is a paragraph. More text for this paragraph.</p>
   <br>
   <img src="https://physicsworld.com/wp-content/uploads/2018/04/65-12-163-FEYNMAN.jpg" alt="Feynman" width="635" height="650">
+  <br>
+  <p> This paragraph is written just to have more text </p>
+  <p> I hate lorem ipsum generated text, so I prefer to write this nonsense here </p>
   </body>
   `
 	w.Write([]byte(mypage))
